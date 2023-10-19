dbcd728f93c15914c5738cb3eaed054a2c223d78
HaZ3R0 <111962660+ouo2u@users.noreply.github.com>
2023-10-19 11:24:27 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿string input = "*+12-34"; //"(1+2) * (3-4)"
+
+Stack<int> stack = new Stack<int>();
+
+for(int i = input.Length-1; i >= 0;)
+{
+    if(input[i] == '+')
+    {
+        int a = stack.Pop();
+        int b = stack.Pop();
+        stack.Push(a+b);
+    }
+    else if(input[i] == '-')
+    {
+        int a = stack.Pop();
+        int b = stack.Pop();
+        stack.Push(a-b);
+    }
+    else if(input[i] == '/')
+    {
+        int a = stack.Pop();
+        int b = stack.Pop();
+        stack.Push(a/b);
+    }
+    else if(input[i] == '*')
+    {
+        int a = stack.Pop();
+        int b = stack.Pop();
+        stack.Push(a*b);
+    }
+    else{
+        int num = int.Parse(input[i].ToString());
+        stack.Push(num);
+    }
+}
+Console.WriteLine(stack.Pop());
